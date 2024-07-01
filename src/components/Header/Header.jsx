0264f0a12712de412ca44a037ff77c426868ecaa
lazeez7 <abdulazizsultan033@gmail.com>
2024-07-01 18:45:38 +0500

    fix all

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,24 +1,81 @@
 import React from "react";
-import './header.css'
+import "./header.css";
+// import component 👇
+import Drawer from "react-modern-drawer";
+
+//import styles 👇
+import "react-modern-drawer/dist/index.css";
+
 const Header = () => {
+  const [isOpen, setIsOpen] = React.useState(false);
+  const toggleDrawer = () => {
+    setIsOpen((prevState) => !prevState);
+  };
   return (
     <nav>
-     <div className="container">
-     <div className="header__wrap">
-        <div className="logo">
-          <img src="./img.png" alt="" />
-        </div>
-        <div className="header__nav">
-          <a href="#">Главная</a>
-          <a href="#">Услуги</a>
-          <a href="#">Наши работы</a>
-          <a href="#">Контакты</a>
-        </div>
-        <div className="header__button">
-          <button className="btn">Заказать звонок</button>
+      <div className="container">
+        <div className="header__wrap">
+          <div className="logo">
+            <img src="./img.png" alt="" />
+          </div>
+          <div className="header__nav">
+            <a href="#">Главная</a>
+            <a href="#">Услуги</a>
+            <a href="#">Наши работы</a>
+            <a href="#">Контакты</a>
+          </div>
+          <div className="header__button">
+            <button className="btn">Заказать звонок</button>
+          </div>
+          <div className="mobile">
+            <svg
+              onClick={toggleDrawer}
+              xmlns="http://www.w3.org/2000/svg"
+              width="36"
+              height="36"
+              fill="currentColor"
+              className="bi bi-list"
+              viewBox="0 0 16 16"
+            >
+              <path
+                fillRule="evenodd"
+                d="M2.5 12a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5m0-4a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5m0-4a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5"
+              />
+            </svg>
+            <Drawer
+              open={isOpen}
+              onClose={toggleDrawer}
+              direction="right"
+              size={"70%"}
+              className="bla bla bla"
+            >
+              <div>
+                <div className="nav_mob">
+                  <h2 onClick={toggleDrawer}>
+                    <svg
+                      xmlns="http://www.w3.org/2000/svg"
+                      width="36"
+                      height="36"
+                      fill="currentColor"
+                      class="bi bi-x"
+                      viewBox="0 0 16 16"
+                    >
+                      <path d="M4.646 4.646a.5.5 0 0 1 .708 0L8 7.293l2.646-2.647a.5.5 0 0 1 .708.708L8.707 8l2.647 2.646a.5.5 0 0 1-.708.708L8 8.707l-2.646 2.647a.5.5 0 0 1-.708-.708L7.293 8 4.646 5.354a.5.5 0 0 1 0-.708" />
+                    </svg>
+                  </h2>
+                </div>
+                <div className="mobile_links">
+                  <a href="#">Главная</a>
+                  <a href="#">Услуги</a>
+                  <a href="#">Наши работы</a>
+                  <a href="#">Контакты</a>
+                  <button className="btn">Заказать звонок</button>
+                </div>
+              </div>
+            </Drawer>
+          </div>
         </div>
       </div>
-     </div>
     </nav>
   );
 };
--- src/components/Services/Services.jsx
+++ src/components/Services/Services.jsx
@@ -2,15 +2,18 @@ import React from "react";
 import s from "./services.module.css";
 
 const Services = () => {
+
   return (
     <>
       <section className={s.services}>
-        <h2 className={s.main_title}>Наши услуги</h2>
         <div className="container">
+          <h2 className={s.main_title}>Наши услуги</h2>
           <div className={s.wrap}>
-              <div className={s.card}>
-                <div className={s.box}>
-                <h4>Рекламные материалы</h4>
+            <div className={s.card}>
+              <div className={s.box}>
+                <div className="title">
+                  <h4>Рекламные материалы</h4>
+                </div>
                 <ul>
                   <li>Верстка макетов</li>
                   <li>Флаеры</li>
@@ -18,16 +21,22 @@ const Services = () => {
                   <li>Брошюры</li>
                   <li>Листовки</li>
                 </ul>
-                <h3> от <span>2000 руб.</span></h3>
-                </div>
-                
-                <button>Подробнее</button>
-
+                <h3>
+                  {" "}
+                  от <span>2000 руб.</span>
+                </h3>
               </div>
 
-              <div className={s.card}>
-                <div className={s.box}>
-                <h4> бренд <br />дизайн</h4>
+              <button>Подробнее</button>
+            </div>
+
+            <div className={s.card}>
+              <div className={s.box}>
+                <h4>
+                  {" "}
+                  бренд <br />
+                  дизайн
+                </h4>
                 <ul>
                   <li>Логотипы</li>
                   <li>Фирменный стиль</li>
@@ -35,45 +44,56 @@ const Services = () => {
                   <li>Концепция бренда</li>
                   <li>Гайдлайн</li>
                 </ul>
-                <h3> от <span>3000 руб.</span></h3>
-                </div>
-                
-                <button>Подробнее</button>
-
+                <h3>
+                  {" "}
+                  от <span>3000 руб.</span>
+                </h3>
               </div>
 
-              <div className={s.card}>
-                <div className={s.box}>
-                <h4>дизайн вывесок<br /> и рекламы</h4>
+              <button>Подробнее</button>
+            </div>
+
+            <div className={s.card}>
+              <div className={s.box}>
+                <h4>
+                  дизайн вывесок
+                  <br /> и рекламы
+                </h4>
                 <ul>
                   <li>Дизайн вывесок</li>
                   <li>Фотопривязки</li>
                   <li>Роллапы</li>
                 </ul>
-                <h3> от <span> 3000 руб.</span></h3>
-                </div>
-                
-                <button>Подробнее</button>
-
+                <h3>
+                  {" "}
+                  от <span> 3000 руб.</span>
+                </h3>
               </div>
 
-              <div className={s.card}>
-                <div className={s.box}>
-                <h4>Digital <br /> Дизайн</h4>
+              <button>Подробнее</button>
+            </div>
+
+            <div className={s.card}>
+              <div className={s.box}>
+                <h4>
+                  Digital <br /> Дизайн
+                </h4>
                 <ul>
                   <li>WEB дизайн</li>
                   <li>Креативы для рекламы</li>
                   <li>Презентации</li>
                 </ul>
-                <h3> от <span>5000 руб.</span></h3>
-                </div>
-                
-                <button>Подробнее</button>
-
+                <h3>
+                  {" "}
+                  от <span>5000 руб.</span>
+                </h3>
               </div>
 
-              <div className={s.card}>
-                <div className={s.box}>
+              <button>Подробнее</button>
+            </div>
+
+            <div className={s.card}>
+              <div className={s.box}>
                 <h4>Smm</h4>
                 <ul>
                   <li>Stories Instagram</li>
@@ -82,12 +102,14 @@ const Services = () => {
                   <li>Рекламные креативы</li>
                   <li>Оформление группы</li>
                 </ul>
-                <h3> от <span>1500 руб.</span></h3>
-                </div>
-                
-                <button>Подробнее</button>
-
+                <h3>
+                  {" "}
+                  от <span>1500 руб.</span>
+                </h3>
               </div>
+
+              <button>Подробнее</button>
+            </div>
           </div>
         </div>
       </section>
